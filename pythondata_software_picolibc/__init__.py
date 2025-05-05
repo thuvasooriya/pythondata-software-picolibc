3b2d5ad5582f7d9986863c17cba02dd38ff74b27
tony <74165167+thuvasooriya@users.noreply.github.com>
2025-05-05 10:22:24 +0530

    update picolibc

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -1,48 +1,48 @@
 import os.path
+
 __dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
 data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.9.post181"
-version_tuple = (1, 7, 9, 181)
+version_str = "1.8.10.post0"
+version_tuple = (1, 8, 10, 0)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.9.post181")
+
+    pversion = V("1.8.10.post0")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.9.post39"
-data_version_tuple = (1, 7, 9, 39)
+data_version_str = "1.8.10.post0"
+data_version_tuple = (1, 8, 10, 0)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.9.post39")
+
+    pdata_version = V("1.8.10.post0")
 except ImportError:
     pass
-data_git_hash = "f165dc22f1f67e3e8bdc8edf750ff7dc596de2ff"
+data_git_hash = "51a8b32857e75345c37652a80b5cda98b28d69e5"
 data_git_describe = "1.7.9-39-gf165dc22f"
 data_git_msg = """\
-commit f165dc22f1f67e3e8bdc8edf750ff7dc596de2ff
+commit 51a8b32857e75345c37652a80b5cda98b28d69e5
 Author: Keith Packard <keithp@keithp.com>
-Date:   Sat Nov 5 19:30:51 2022 -0700
+Date: Mon Apr 14 09:50:00 2025 - 0700
 
-    Switch read/write to posix types
-    
-    Remove legacy cygwin hacks which set the buffer size and return value
-    for read and write to int, switch them to the correct POSIX types,
-    size_t for buffer size and ssize_t for the return value.
-    
+    Version 1.8.10
+
     Signed-off-by: Keith Packard <keithp@keithp.com>
 
 """
 
 # Tool version info
-tool_version_str = "0.0.post142"
-tool_version_tuple = (0, 0, 142)
+tool_version_str = "0.0.post143"
+tool_version_tuple = (0, 0, 143)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post142")
+
+    ptool_version = V("0.0.post143")
 except ImportError:
     pass
 
@@ -52,5 +52,5 @@ def data_file(f):
     fn = os.path.join(data_location, f)
     fn = os.path.abspath(fn)
     if not os.path.exists(fn):
-        raise IOError("File {f} doesn't exist in pythondata_software_picolibc".format(f))
+        raise IOError("File {f} doesn't exist in pythondata_software_picolibc")
     return fn
